daef0de2a4c5af344988c087ba3cc0916fb8c579
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2024-01-15 17:30:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-function preload() {
+video = "" ;
 
+function preload() {
+    video = createVideo("video.mp4") ;
+    video.hide() ;
 }
 
 function setup() {
-    canvas = createCanvas(350 , 350) ;
-    canvas.position(500 , 70)
+    canvas = createCanvas(480 , 380) ;
+    canvas.position(450 , 40) ;
+}
+
+function draw() {
+    image(video , 0 , 0 , 480 , 380) ;
+}
+
+function start() {
+    objectDetector = ml5.objectDetector("cocossd" , modelLoaded) ;
+    document.getElementById("status").innerHTML = "Status : Detecting Object" ;
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!") ;
+    status = true ;
+    video.loop() ;
+    video.speed(1) ;
+    video.volume(0) ;
 }
