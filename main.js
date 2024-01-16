e3f72dd52e2d97bd960be07f27bb294d02a38625
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2024-01-16 18:40:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 video = "" ;
+status = "" ;
+objects = [] ;
+
 
 function preload() {
     video = createVideo("video.mp4") ;
@@ -12,6 +15,28 @@ function setup() {
 
 function draw() {
     image(video , 0 , 0 , 480 , 380) ;
+    if(status != "") {
+        objectDetector.detect(video , gotResults) ;
+        for(i = 0 ; i < objects.length ; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected" ;
+            document.getElementById("number_of_objects").innerHTML = "Objects Detected are : " + objects.length ;
+
+            stroke('#0000FF') ;
+            fill('#0000FF') ;
+            noFill() ;
+            percent = floor(objects[i].confidence * 100) ;
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 15 , objects[i].y + 15) ;
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height) ;
+        }
+    }
+}
+
+function gotResults(error , results) {
+    if(error) {
+        console.error(error) ;
+    }
+    console.log(results) ;
+    objects = results ;
 }
 
 function start() {
